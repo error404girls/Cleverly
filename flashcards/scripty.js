e8edbbd0de0e514685406d3d3608e445f216aed8
error404girls <xxshamanzgx@gmail.com>
2026-01-14 20:59:23 +0200

    Update scripty.js

--- flashcards/scripty.js
+++ flashcards/scripty.js
@@ -39,7 +39,7 @@ function toggleDropdown() {
     try 
     {
       const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyB_WoqniusYawIVp1l5ueJUD06zuCaIr2o",
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=--",
         {
           method: "POST",
           headers: {
